5e2818edf08cc407b3feee8e5f0b6817569ce355
Bradley Ashley <HAISBMRME@gmail.com>
2020-01-24 15:01:27 +0000

    Added Base Character Movement

---
+++ Assets/Scripts/PlayerMovement/CharacterMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterMotor : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool isControlled = false;
+
+    public void TakenOver()
+    {
+        isControlled = true;
+    }
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void MovementMotor(Vector2 movementvect)
+    {
+        rb.velocity = movementvect;
+    }
+
+    public void CharactorRotator(Vector2 rotationVector)
+    {
+        transform.Rotate(rotationVector);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : Player
+{
+
+    void Update()
+    {
+        GlobalManager();
+        ControlerManger();
+    }
+
+    void GlobalManager()
+    {
+
+        if (controlledObject != lastControlled)
+        {
+            controlledObject.GetComponent<CharacterMotor>().TakenOver();
+        }
+
+    }
+
+    void ControlerManger()
+    {
+        float Xvect = Input.GetAxisRaw("Horizontal");
+        float Yvect = Input.GetAxisRaw("Vertical");
+        controlledObject.GetComponent<CharacterMotor>().MovementMotor((new Vector2(Xvect,Yvect)) * characterSpeed); ;
+    }
+}
