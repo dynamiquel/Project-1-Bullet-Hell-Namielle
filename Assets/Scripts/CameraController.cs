b4dc8aaee0c72ce16a1a0baa3db06627e8bfc00c
D.Bickerdike U1963266 <U1963266@unimail.hud.ac.uk>
2020-01-31 15:00:52 +0000

    First iteration of the GeneralAI class, added Idle Pivot functionality but currently does not use the CharacterMotor. Check the project requirements for more info

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,11 +11,6 @@ public class CameraController : MonoBehaviour
     public GameObject PlayerMotor;
     public float speed = 15f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,29 +18,3 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, new Vector3(PlayerMotor.transform.position.x, PlayerMotor.transform.position.y, -10), Time.deltaTime * speed);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// First version created by: Daniel Bickerdike
-// Super simple Camera using Vector3's Lerp() method, could include clamps later on...
-
-public class CameraController : MonoBehaviour
-{
-    public GameObject PlayerMotor;
-    public float speed = 15f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(PlayerMotor.transform.position.x, PlayerMotor.transform.position.y, -10), Time.deltaTime * speed);
-    }
-}
->>>>>>> bac0b48de3c5dc7e63d374e5c823f8327b7fb46a
