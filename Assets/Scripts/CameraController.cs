9c92ee4d6a8f828581cc6249145720e27cc82044
Bradley Ashley <26680536+HAISBMRME@users.noreply.github.com>
2020-02-14 15:01:51 +0000

    Added Art file and the GameV1 secene
    Joshua Harrison Did this

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,25 +1,13 @@
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// First version created by: Daniel Bickerdike
-// Super simple Camera using Vector3's Lerp() method, could include clamps later on...
-
 public class CameraController : MonoBehaviour
 {
-    public GameObject PlayerMotor;
-    public float speed = 15f;
+    public Transform playerTransform;
 
-    private void Start()
+    private void Update()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(PlayerMotor.transform.position.x, PlayerMotor.transform.position.y, -10), Time.deltaTime * speed);
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
     }
 }
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 leftStickControl;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerInput();
+    }
+
+    private void GetPlayerInput()
+    {
+        leftStickControl = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 curMovement = leftStickControl * playerSpeed * Time.deltaTime;
+
+        rb.MovePosition(rb.position + curMovement);
+    }
+}
